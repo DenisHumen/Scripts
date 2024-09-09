7b0046f740cc4a41b234d1da5aa02a472af66ed8
Krokosha <56129242+MeSmallMan@users.noreply.github.com>
2024-09-09 13:25:11 +0300

    Create install_allora.sh

---
+++ install_allora.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+echo "-----------------------------------------------------------------------------"
+curl -s https://raw.githubusercontent.com/MeSmallMan/logo/main/logo0.sh | bash
+echo "-----------------------------------------------------------------------------"
+
+echo "-----------------------------------------------------------------------------"
+echo "Установка Allora Worker - Basic Coin Prediction Node"
+echo "-----------------------------------------------------------------------------"
+
+source $HOME/.profile
+
+if [ -z "$ALLORA_SEED_PHRASE" ]; then
+    echo "Введите сид фразу от кошелька, который будет использоваться для воркера"
+    read ALLORA_SEED_PHRASE
+    echo "export ALLORA_SEED_PHRASE='$ALLORA_SEED_PHRASE'" >> $HOME/.profile
+fi
+
+cd $HOME
+git clone https://github.com/allora-network/basic-coin-prediction-node
+cd basic-coin-prediction-node
+rm -rf config.json
+
+wget https://raw.githubusercontent.com/MeSmallMan/allora/main/config.json
+sed -i "s|SeedPhrase|$ALLORA_SEED_PHRASE|" $HOME/basic-coin-prediction-node/config.json
+
+chmod +x init.config
+./init.config
+
+sed -i "s|\"8000:8000|\"18000:8000|" $HOME/basic-coin-prediction-node/docker-compose.yml
+sed -i "s|intervals = [\"1d\"]|intervals = [\"10m\", \"20m\", \"1h\", \"1d\"]|" $HOME/basic-coin-prediction-node/model.py
+
+sudo tee $HOME/basic-coin-prediction-node/.env > /dev/null <<EOF
+TOKEN=ETH
+TRAINING_DAYS=30
+TIMEFRAME=4h
+MODEL=LinearRegression
+REGION=EU
+DATA_PROVIDER=Binance
+CG_API_KEY=
+EOF
+
+sleep 5
+
+docker compose up -d --build
+
+echo "-----------------------------------------------------------------------------"
+curl -shttps://raw.githubusercontent.com/MeSmallMan/logo/main/logo0.sh | bash
+echo "-----------------------------------------------------------------------------"
