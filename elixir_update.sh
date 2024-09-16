35735570072178ef0c31ed43ce3ad5077455bf0c
Krokosha <56129242+MeSmallMan@users.noreply.github.com>
2024-09-16 10:05:43 +0300

    Create elixir_update.sh

---
+++ elixir_update.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+curl -s https://raw.githubusercontent.com/MeSmallMan/logo/main/logo0.sh | bash
+
+
+docker stop elixir && docker rm elixir
+docker pull elixirprotocol/validator:v3
+docker run -d --env-file /root/validator.env --name elixir --restart unless-stopped elixirprotocol/validator:v3
