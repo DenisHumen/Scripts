c0242be296ff823166d5bb213dc7a556a438b487
Krokosha <56129242+MeSmallMan@users.noreply.github.com>
2024-09-17 12:03:58 +0300

    Update elixir_update.sh

--- elixir_update.sh
+++ elixir_update.sh
@@ -7,4 +7,4 @@ docker stop elixir && docker rm elixir
 docker pull elixirprotocol/validator:v3
 docker run -d --env-file /root/validator.env --name elixir --restart unless-stopped -p 17690:17690 elixirprotocol/validator:v3
 
-echo "<3"
+echo "Thanks <3"
